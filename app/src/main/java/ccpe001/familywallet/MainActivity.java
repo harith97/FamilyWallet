5ac43e1c9103cab40d36cc36715a39a4863633b0
Gihan Kumarasinghe <tharukakumarasinghesg@gmail.com>
2017-04-06 13:42:46 +0530

    test no3

--- app/src/main/java/ccpe001/familywallet/MainActivity.java
+++ app/src/main/java/ccpe001/familywallet/MainActivity.java
@@ -3,7 +3,7 @@ package ccpe001.familywallet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
-/*this is what*/
+/*this is  family wallet project*/
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
