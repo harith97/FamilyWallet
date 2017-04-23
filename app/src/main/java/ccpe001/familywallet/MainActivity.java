5d8af8feaaf8218750d9ee4a1911336b3d3d2919
rmsr-knight <rmsrknight@gmail.com>
2017-04-23 16:05:33 +0530

    app icon

--- app/src/main/java/ccpe001/familywallet/MainActivity.java
+++ app/src/main/java/ccpe001/familywallet/MainActivity.java
@@ -3,6 +3,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
+    //test
 
     // Used to load the 'native-lib' library on application startup.
     static {
