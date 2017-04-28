09687097b6b6ada92812b07c069e37d66b57e543
ccpe001 <ccpe001@gmail.com>
2017-04-28 14:26:05 +0530

    ongoing interfaces

--- app/src/main/java/ccpe001/familywallet/admin/Login.java
+++ app/src/main/java/ccpe001/familywallet/admin/Login.java
@@ -1,8 +1,34 @@
 package ccpe001.familywallet.admin;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import ccpe001.familywallet.R;
+
 /**
  * Created by Knight on 4/23/2017.
  */
 
-public class Login {
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+        init();
+    }
+
+    private void init() {
+    }
 }
+
+/*<activity android:name=".MainActivity"
+        android:windowSoftInputMode="adjustPan">
+<intent-filter>
+<action android:name="android.intent.action.MAIN" />
+
+<category android:name="android.intent.category.LAUNCHER" />
+</intent-filter>
+</activity>*/
+
+//<action android:name="ccpe001.familywallet.LOGIN" />
+//<category android:name="android.intent.category.DEFAULT" />
