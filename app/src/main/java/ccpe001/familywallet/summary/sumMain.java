665c30cb07b5902ca0b118a8df6ba7f937818633
Gigum Bandara Rajaguru <gigumbrajaguru@gmail.com>
2017-04-30 14:31:26 +0530

    First summary layout

---
+++ app/src/main/java/ccpe001/familywallet/summary/sumMain.java
@@ -0,0 +1,15 @@
+package ccpe001.familywallet.summary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ccpe001.familywallet.R;
+
+public class sumMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sum_main);
+    }
+}
