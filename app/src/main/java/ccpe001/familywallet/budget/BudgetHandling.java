cdadaefa3809ec730c01bed0edc9eadb3e7bc4d1
Gigum Bandara Rajaguru <gigumbrajaguru@gmail.com>
2017-05-02 23:34:17 +0530

    Budget interfaces patch 3

--- app/src/main/java/ccpe001/familywallet/budget/BudgetHandling.java
+++ app/src/main/java/ccpe001/familywallet/budget/BudgetHandling.java
@@ -2,14 +2,60 @@ package ccpe001.familywallet.budget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.EditText;
+import android.app.DatePickerDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import java.util.Calendar;
 import ccpe001.familywallet.R;
 
-public class BudgetHandling extends AppCompatActivity {
+public class BudgetHandling extends AppCompatActivity implements View.OnClickListener {
+    EditText strDt,endDt;
+    private  int day,mon,yr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.budget_handling);
+        strDt=(EditText)findViewById(R.id.startDate);
+        endDt=(EditText)findViewById(R.id.endDate);
+        strDt.setOnClickListener(this);
+        endDt.setOnClickListener(this);
+
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if(v==strDt){
+            final Calendar c= Calendar.getInstance();
+            day=c.get(Calendar.DAY_OF_MONTH);
+            mon=c.get(Calendar.MONTH);
+            yr=c.get(Calendar.YEAR);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    strDt.setText(dayOfMonth+"/"+(monthOfYear+1)+"/"+year);
+                }
+            }
+                    ,day,mon,yr);
+            datePickerDialog.show();
+        }
+        if (v==endDt){
+            final Calendar c= Calendar.getInstance();
+            day=c.get(Calendar.DAY_OF_MONTH);
+            mon=c.get(Calendar.MONTH);
+            yr=c.get(Calendar.YEAR);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    endDt.setText(dayOfMonth+"/"+(monthOfYear+1)+"/"+year);
+                }
+            },day,mon,yr);
+            datePickerDialog.show();
+        }
     }
 }
--- app/src/main/java/ccpe001/familywallet/budget/budgetUpdate.java
+++ app/src/main/java/ccpe001/familywallet/budget/budgetUpdate.java
@@ -1,15 +1,59 @@
 package ccpe001.familywallet.budget;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Calendar;
 
 import ccpe001.familywallet.R;
 
-public class budgetUpdate extends AppCompatActivity {
-
+public class budgetUpdate extends AppCompatActivity implements View.OnClickListener  {
+    EditText strDt,endDt;
+    private  int day,mon,yr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.budget_update);
+        strDt=(EditText)findViewById(R.id.startDate);
+        endDt=(EditText)findViewById(R.id.endDate);
+        strDt.setOnClickListener(this);
+        endDt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==strDt){
+            final Calendar c= Calendar.getInstance();
+            day=c.get(Calendar.DAY_OF_MONTH);
+            mon=c.get(Calendar.MONTH);
+            yr=c.get(Calendar.YEAR);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    strDt.setText(dayOfMonth+"/"+(monthOfYear+1)+"/"+year);
+                }
+            }
+                    ,day,mon,yr);
+            datePickerDialog.show();
+        }
+        if (v==endDt){
+            final Calendar c= Calendar.getInstance();
+            day=c.get(Calendar.DAY_OF_MONTH);
+            mon=c.get(Calendar.MONTH);
+            yr=c.get(Calendar.YEAR);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    endDt.setText(dayOfMonth+"/"+(monthOfYear+1)+"/"+year);
+                }
+            },day,mon,yr);
+            datePickerDialog.show();
+        }
     }
 }
