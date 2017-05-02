27e2d08770af5d89bef24d909163ed6ec5176b94
Gigum Bandara Rajaguru <gigumbrajaguru@gmail.com>
2017-05-02 18:46:48 +0530

    budget interfaces patch 02

---
+++ app/src/main/java/ccpe001/familywallet/budget/budgetTrack.java
@@ -0,0 +1,15 @@
+package ccpe001.familywallet.budget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ccpe001.familywallet.R;
+
+public class budgetTrack extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.budget_track);
+    }
+}
--- app/src/main/java/ccpe001/familywallet/summary/sumMain.java
+++ app/src/main/java/ccpe001/familywallet/summary/sumMain.java
@@ -1,10 +1,7 @@
 package ccpe001.familywallet.summary;
 
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
