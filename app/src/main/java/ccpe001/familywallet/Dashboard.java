43ec99476809170fab2befaf44ca87d6b9208297
ccpe001 <ccpe001@gmail.com>
2017-05-12 16:53:23 +0530

    done

--- app/src/main/java/ccpe001/familywallet/Dashboard.java
+++ app/src/main/java/ccpe001/familywallet/Dashboard.java
@@ -12,19 +12,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import at.markushi.ui.CircleButton;
+import ccpe001.familywallet.budget.accUpdate;
+import ccpe001.familywallet.budget.addAccount;
+import ccpe001.familywallet.budget.budgetList;
 import ccpe001.familywallet.summary.sumMain;
 
 public class Dashboard extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,View.OnClickListener{
 
     Toolbar toolbar = null;
     NavigationView navigationView = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_drawer);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -37,11 +47,11 @@ public class Dashboard extends AppCompatActivity
         });
 
         //initialize dashboard fragment 1st
-        android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+        /*android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
         android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        transactionMain12 transaction = new transactionMain12();
+        transactionMain transaction = new transactionMain();
         fragmentTransaction.replace(R.id.fragmentContainer1,transaction);
-        fragmentTransaction.commit();
+        fragmentTransaction.commit();*/
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -51,6 +61,11 @@ public class Dashboard extends AppCompatActivity
 
         navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        View headerView = navigationView.inflateHeaderView(R.layout.nav_header_navigation_drawer);
+        CircleButton img = (CircleButton)headerView.findViewById(R.id.loggedUsrImg);
+        img.setOnClickListener(this);
+
+
     }
 
     @Override
@@ -92,33 +107,52 @@ public class Dashboard extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.transactionFrag) {
-            /*android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
-            android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            transactionMain12 transaction = new transactionMain12();
-            fragmentTransaction.replace(R.id.fragmentContainer1,transaction);
-            fragmentTransaction.commit();*/
-        } else if (id == R.id.nav_gallery) {
+             /*android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+             android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+             Dashboard dashboard = new Dashboard();
+             fragmentTransaction.replace(R.id.fragmentContainer1,dashboard);
+             fragmentTransaction.commit();*/
+        } else if (id == R.id.reportsFrag) {
             android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
             android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
             sumMain summary = new sumMain();
             fragmentTransaction.replace(R.id.fragmentContainer1,summary);
             fragmentTransaction.commit();
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.transferFrag) {
             android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
             android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            sumMain summary = new sumMain();
-            fragmentTransaction.replace(R.id.fragmentContainer1,summary);
+            accUpdate transmoney = new accUpdate();
+            fragmentTransaction.replace(R.id.fragmentContainer1,transmoney);
             fragmentTransaction.commit();
-        }else if (id == R.id.nav_slideshow) {
+        }else if (id == R.id.budgetFrag) {
             android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
             android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            Settings setting = new Settings();
-            fragmentTransaction.replace(R.id.fragmentContainer1,setting);
+            budgetList budget = new budgetList();
+            fragmentTransaction.replace(R.id.fragmentContainer1,budget);
+            fragmentTransaction.commit();
+        }else if (id == R.id.walletFrag) {
+            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+            android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            addAccount addwallet = new addAccount();
+            fragmentTransaction.replace(R.id.fragmentContainer1,addwallet);
             fragmentTransaction.commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId()==R.id.loggedUsrImg){
+            android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
+            android.support.v4.app.FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            AddMember addmember = new AddMember();
+            fragmentTransaction.replace(R.id.fragmentContainer1,addmember);
+            fragmentTransaction.commit();
+            //Close nav drawer here
+        }
     }
 }
