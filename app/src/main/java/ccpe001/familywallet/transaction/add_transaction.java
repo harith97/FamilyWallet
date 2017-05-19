d3982ca734956f4dac63f7c0e2a20bf7d4751a78
rmsr-knight <rmsrknight@gmail.com>
2017-05-19 21:40:10 +0530

    5/19-9.39pm

--- app/src/main/java/ccpe001/familywallet/transaction/add_transaction.java
+++ app/src/main/java/ccpe001/familywallet/transaction/add_transaction.java
@@ -1,15 +1,93 @@
 package ccpe001.familywallet.transaction;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 import ccpe001.familywallet.R;
 
 public class add_transaction extends AppCompatActivity {
 
+
+    private TextView txtLocation;
+    int PLACE_PICKER_REQUEST=1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_transaction);
+        txtLocation = (TextView)findViewById(R.id.txtLocation);
+        txtLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPlacePickerActivity();
+            }
+        });
+    }
+
+
+    private void startPlacePickerActivity() {
+        PlacePicker.IntentBuilder intentBuilder = new PlacePicker.IntentBuilder();
+        // this would only work if you have your Google Places API working
+
+        try {
+            Intent intent = intentBuilder.build(this);
+            startActivityForResult(intent, PLACE_PICKER_REQUEST);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void displaySelectedPlaceFromPlacePicker(Intent data) {
+        Place placeSelected = PlacePicker.getPlace(data, this);
+
+        String name = placeSelected.getName().toString();
+        String address = placeSelected.getAddress().toString();
+
+        //TextView enterCurrentLocation = (TextView) findViewById(R.id.txtLocation);
+        txtLocation.setText(name + ", " + address);
+    }
+
+    @Override
+    protected  void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == PLACE_PICKER_REQUEST && resultCode == RESULT_OK) {
+            displaySelectedPlaceFromPlacePicker(data);
+        }
+    }
+
+    public void onStart(){
+        super.onStart();
+        TextView txtDate = (TextView) findViewById(R.id.txtDate);
+        txtDate.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+            @Override
+            public void onFocusChange(View v, boolean hasFocus){
+                if(hasFocus){
+                    DateDialog dialog = new DateDialog(v);
+                    android.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    dialog.show(ft,"DatePicker");
+                }
+            }
+        });
+
+        TextView txtTime = (TextView) findViewById(R.id.txtTime);
+        txtTime.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+            @Override
+            public void onFocusChange(View v, boolean hasFocus){
+                if(hasFocus){
+                    TimeDialog dialog = new TimeDialog(v);
+                    android.app.FragmentTransaction ft = getFragmentManager().beginTransaction();
+                    dialog.show(ft,"TimePicker");
+                }
+            }
+        });
     }
 }
